9cc08cb7e00f7e1a73a65ff97d6a12dde051fdf6
rosarafael00 <113437955+rosarafael00@users.noreply.github.com>
2022-11-23 19:47:02 -0300

    Criação da Classe Inimigos

--- lib/Inimigos.hpp
+++ lib/Inimigos.hpp
@@ -1,4 +1,15 @@
 #ifndef INIMIGOS_H
 #define INIMIGOS_H
 
+#include "../lib/Personagem.hpp"
+
+class Inimigos : public Personagem
+{
+private:
+    std::string fraqueza;
+
+public:
+    Inimigos(std::string _nome);
+};
+
 #endif

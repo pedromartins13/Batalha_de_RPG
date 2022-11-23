bf8174efee5e8db09b51fd1ab0fadd58c0f68525
pedromartins13 <115956693+pedromartins13@users.noreply.github.com>
2022-11-23 18:30:21 -0300

    Criação da nova pasta src junto do personagem.cpp

---
+++ src/Personagem..cpp
@@ -0,0 +1,101 @@
+#include "../lib/Personagem.hpp"
+
+Personagem::Personagem(std::string nome, std::string _historia, int _DES, int _INT, int _CON, int _FOR, Classe *_Classe)
+{
+    this->nome = nome;
+    this->historia = _historia;
+    this->Classe_Personagem = _Classe;
+
+    this->DES = this->Classe_Personagem->getMDES() * _DES;
+    this->INT = this->Classe_Personagem->getMINT() * _INT;
+    this->CON = this->Classe_Personagem->getMCON() * _CON;
+    this->FOR = this->Classe_Personagem->getMFOR() * _FOR;
+
+    this->ataque = this->FOR * 5;
+    this->defesa = this->DES * 5;
+    this->vida = this->CON * 5;
+    this->mana = this->INT * 5;
+
+    this->level = 1;
+    this->experiencia = 0;
+};
+
+Personagem::~Personagem(){};
+void Personagem::setVida(int _vida)
+{
+    this->vida = _vida;
+};
+
+void Personagem::setMana(int _mana)
+{
+    this->mana = _mana;
+};
+void Personagem::setEXP(int _exp)
+{
+    this->experiencia = _exp;
+}
+void Personagem::setAtaque(int _atq)
+{
+    this->ataque = _atq;
+}
+void Personagem::setDefesa(int _def)
+{
+    this->defesa = _def;
+}
+void Personagem::setDES(int _des)
+{
+    this->DES = _des;
+}
+void Personagem::setINT(int _int)
+{
+    this->INT = _int;
+}
+void Personagem::setCON(int _con)
+{
+    this->CON = _con;
+}
+void Personagem::setClasse(Classe *_classe)
+{
+    this->Classe_Personagem = _classe;
+}
+
+int Personagem::getVida()
+{
+    return this->vida;
+}
+int Personagem::getMana()
+{
+    return this->mana;
+}
+int Personagem::getEXP()
+{
+    return this->experiencia;
+}
+int Personagem::getAtaque()
+{
+    return this->ataque;
+}
+int Personagem::getDefesa()
+{
+    return this->defesa;
+}
+int Personagem::getDES()
+{
+    return this->DES;
+}
+int Personagem::getINT()
+{
+    return this->INT;
+}
+int Personagem::getCON()
+{
+    return this->CON;
+}
+int Personagem::getLVL()
+{
+    return this->level;
+}
+Classe *Personagem::getClasse()
+{
+    return this->Classe_Personagem;
+}

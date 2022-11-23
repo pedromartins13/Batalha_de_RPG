077b45b7951d65334a868180674d28ee097cbc0b
rosarafael00 <113437955+rosarafael00@users.noreply.github.com>
2022-11-23 19:50:05 -0300

    Inicio teste para a main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include "../lib/Classe.hpp"
 #include "../lib/Personagem.hpp"
 #include "../lib/Interface.hpp"
+#include "../lib/Inimigos.hpp"
+#include "../lib/Turno.hpp"
+#include <vector>
 
+int opcaoMenu = 0;
 int main()
 {
+    std::vector<Inimigos*> Lista_Inimigos;
+    std::vector<Inimigos*>::iterator it;
+    Inimigos Goblin("Goblin");
+    Inimigos Zumbi("Zumbi");
+
+    Lista_Inimigos.push_back(&Goblin);
+    Lista_Inimigos.push_back(&Zumbi);
+
     Personagem Jogador = Criar_Personagem();
+    opcaoMenu = menuInicial();
+
+    if (opcaoMenu == 1)
+    {
+        while (Lista_Inimigos.size() != 0)
+        {
+            int i = 0;
+            Luta(Lista_Inimigos[i], &Jogador);
+            Lista_Inimigos.erase(Lista_Inimigos.begin());
+            
+        }
+    }
+
     return 0;
 };

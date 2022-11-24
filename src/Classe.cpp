62c550562814dd56e10b4b3e122480826ae841d9
gustavochavesm <103235169+gustavochavesm@users.noreply.github.com>
2022-11-23 21:35:09 -0300

    Modficação da função ListarAtaques
    Agora a função pega o nome da Classe que chamou ela

--- src/Classe.cpp
+++ src/Classe.cpp
@@ -41,6 +41,35 @@ Classe::Classe()
     this->Multiplicador_FOR = 0;
 };
 
+void Classe::listarAtaques()
+{
+    if (this->nome_da_classe == "Mago")
+    {
+        std::cout << "1 - Ataque Básico" << std::endl
+                  << "2 - Raio" << std::endl
+                  << "3 - Geada" << std::endl
+                  << "4 - Bola de Fogo" << std::endl;
+    }
+    else if (this->nome_da_classe == "Guerreiro")
+    {
+        std::cout << "1 - Ataque Básico" << std::endl
+                  << "2 - Ataque Forte" << std::endl
+                  << "3 - Dança das Espadas" << std::endl
+                  << "4 - Espadada Fatal" << std::endl;
+    }
+    else if (this->nome_da_classe == "Ladino")
+    {
+        std::cout << "1 - Ataque Básico" << std::endl
+                  << "2 - Ataque Furtivo" << std::endl
+                  << "3 - Estrangulamento" << std::endl
+                  << "4 - Adaga Cortante" << std::endl;
+    }
+    else
+    {
+        std::cout << "Classe não Encontrada" << std::endl;
+    }
+}
+
 int Classe::Ataque_1()
 {
     return 5;
